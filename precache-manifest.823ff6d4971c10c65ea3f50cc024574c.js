fcdf9ac124bd91b7b04f61c6f4e751d14c2f65b6
Scott Ratigan <Scott@HoneCaps-MacBook-Pro.local>
2019-10-18 12:54:44 -0700

    Updates

---
+++ precache-manifest.823ff6d4971c10c65ea3f50cc024574c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f506512182716aa053d5a9023d6023",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "9cd539c322b38e4930cc",
+    "url": "/todo-list/static/css/main.45acfaf0.chunk.css"
+  },
+  {
+    "revision": "85bb135ac54676de2416",
+    "url": "/todo-list/static/js/2.70b5d091.chunk.js"
+  },
+  {
+    "revision": "9cd539c322b38e4930cc",
+    "url": "/todo-list/static/js/main.2d7c93d3.chunk.js"
+  },
+  {
+    "revision": "da715ee0b4f01398cebe",
+    "url": "/todo-list/static/js/runtime-main.14b3a6b5.js"
+  }
+]);
